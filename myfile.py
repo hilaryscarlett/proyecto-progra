6645660440eb14070c1e1443c3139c07445f710a
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-11-19 17:12:31 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -10,11 +10,19 @@ st.title("Fallecidos por COVID-19")
 
 st.caption("En esta página web se realiza el registro diaria de muertes por Covid-19")
 text=st
-d= st.date_input("fallecidos",datetime.date(2019, 7, 6))
+
+tittle=st.text_input('Nombre y Apellidos:')
+st.write(tittle)
+
+d= st.date_input("Fecha de fallecimiento",datetime.date(2019, 7, 6))
 st.write("ingresar fecha de fallecimiento:", d)
-option = st.selectbox('¿Cómo desearía ser contactado/a?',('Email', 'Teléfono', 'Whatsapp'))
+
+choice = st.selectbox("Sexo", ["Femenino","Masculino"]
+
+
+option = st.selectbox('¿Cómo desearía ser contactado/a?',('Email', 'Teléfono'))
 st.write('Seleccionó:', option)
 
 tittle=st.text_input('Ingrese la opción que eligió')
-
+st.write("Número de teléfono o email de contecto:", tittle)
 
