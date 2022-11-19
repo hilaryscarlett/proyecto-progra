bc3525a8456f2a80246bbc2616dbf3c92ba60190
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-19 12:25:01 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -3,4 +3,15 @@ import pandas as pd
 import numpy as np
 
 st.title("Proyecto Programación 2022-2")
+st.write()
+st.line_chart()
 
+st.title()
+st.write()
+st.line_chart()
+
+st.title()
+x=pd.DataFrame(
+  np.random.randn(1000,2)/[50,50]+[]
+  columns=[])
+st.map(x)
