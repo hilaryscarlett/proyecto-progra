7f8ef84670b26d5cd74186594ec4eeca7f9d8e3e
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-11-19 16:51:39 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -3,15 +3,12 @@ import pandas as pd
 import numpy as np
 from datetime import time
 import datetime
-from PIL import Image
 
-st.header("Adaptadod de Ministerio de Salud")
+
+
 st.title("Fallecidos por COVID-19")
 
-image=Image.open('imag-refe.jpg')
-
-st.image(image,caption='Sunrise by the mountains')
-st.subheader("En esta pagina web se realiza el registro diaria de muertes por Covid-19")
+st.caption("En esta pagina web se realiza el registro diaria de muertes por Covid-19")
 text=st
 d= st.date_input("fallecidos",datetime.date(2019, 7, 6))
 st.write("ingresar fecha de fallecimiento:", d)
