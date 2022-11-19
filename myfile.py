bd621e86ac222b7ddccbe7def47e765669ae6183
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-19 15:42:50 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -5,7 +5,3 @@ import datetime
 
 st.title("Proyecto Programación 2022-2")
 st.title("                Fallecidos por covid              ")
-
-d= st.date_input("ingresar fecha de fallecimiento", datetime.data(2019, 7, 6))
-st.write("fecha de fallecieminto es")
-
