3a0a7235ae4268a587ca49f068879092b469f010
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-19 15:23:01 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Proyecto Programación 2022-2")
+st.title("Proyecto Programación 2022-2")l
+st.title("Fallecidos por covid")
 st.write("hola cons")
 num= st.slider("num", 0, 100, step=1)
 st.write("El numero ingresado es {}".format(hum))
