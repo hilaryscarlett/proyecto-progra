65406dc457296487d23e73539731fe387567d0e9
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-11-19 16:16:09 -0500

    Create myfile.py

--- myfile.py
+++ myfile.py
@@ -7,7 +7,11 @@ import datetime
 
 st.title("Proyecto Programación 2022-2")
 st.title("                Fallecidos por covid              ")
-d= st.date_input("Ingresar fecha de fallecimiento",datetime.date(2019, 7, 6))
-st.write("Fecha de fallecimiento:", d)
+d= st.date_input("fallecidos",datetime.date(2019, 7, 6))
+st.write("ingresar fecha de fallecimiento:", d)
 option = st.selectbox('¿Cómo desearía ser contactado/a?',('Email', 'Teléfono', 'Whatsapp'))
 st.write('Seleccionó:', option)
+
+tittle=st.text_input('Ingrese su número de Teléfono')
+
+
