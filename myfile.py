eddd24dce10911d7d3e9112968665bca39d20860
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-19 15:29:00 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title("Proyecto Programación 2022-2")l
+st.title("Proyecto Programación 2022-2")
 st.title("Fallecidos por covid")
 st.write("hola cons")
 num= st.slider("num", 0, 100, step=1)
-st.write("El numero ingresado es {}".format(hum))
+st.write("El numero ingresado es {}".format(num))
 st.line_chart()
 
 st.title()
