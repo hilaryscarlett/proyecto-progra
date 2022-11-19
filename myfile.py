6114fa0c13145c63d5f3a2e51e3eddcc1e5cfff7
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-19 15:58:12 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -4,7 +4,10 @@ import numpy as np
 from datetime import time
 import datetime
 
+
 st.title("Proyecto Programación 2022-2")
 st.title("                Fallecidos por covid              ")
 d= st.date_input("fallecidos",datetime.date(2019, 7, 6))
 st.write("ingresar fecha de fallecimiento:", d)
+option = st.selectbox('¿Cómo desearía ser contactado/a?',('Email', 'Teléfono', 'Whatsapp'))
+st.write('Seleccionó:', option)
