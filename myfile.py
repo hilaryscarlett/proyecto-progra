cd1555798fc43ee5b18ad32a5193c1bfc2e91525
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-19 15:51:02 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -1,7 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from datetime import time
 import datetime
 
 st.title("Proyecto Programación 2022-2")
 st.title("                Fallecidos por covid              ")
+d= st.date_input("fallecidos",datetime.date(2019, 7, 6))
+st.write("ingresar fecha de fallecimiento:", d)
