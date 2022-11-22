0ecfc5bb03ebc04bbe51173e38c0fad9472ee46a
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-21 20:08:40 -0500

    Update 1234.py

--- 1234.py
+++ 1234.py
@@ -20,5 +20,5 @@ st.write(c.describe())
 
 url= "https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv"
 datos=pd.read_csv(url,sep=",")
-st.line_chart(data=datos,x="FECHA_UTC,"Y="MAGNITUD")
+st.line_chart(data=datos,x="FECHA_UTC",y="MAGNITUD")
 
