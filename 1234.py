32fe0fbcf70f404f48b6160a0d48970f5b37467a
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-21 19:53:24 -0500

    Update 1234.py

--- 1234.py
+++ 1234.py
@@ -7,14 +7,15 @@ st.header("FALLECIDOS COVID")
 
 def download_data():
   url='https://files.minsa.gob.pe/s/t9AFqRbXw3F55Ho/download'
-    filename="fallecidos_covid"
-    urllib.request.urlretrieve(url,filename)
-    df=pd.read_csv("fallecidos_covid")
-    return df
-  c=download_data()
-  st.write("dimensiones: "+str(c.shape[0])+"filas"+str(c.shape[1])+"columnas")
-  st.dataframe(c)
-  st.subheader("caracteristicas del dataset")
-  st.write(c.describe())
+  filename="fallecidos_covid"
+  urllib.request.urlretrieve(url,filename)
+  df=pd.read_csv("fallecidos_covid")
+  return df
+  
+c=download_data()
+st.write("dimensiones: "+str(c.shape[0])+"filas"+str(c.shape[1])+"columnas")
+st.dataframe(c)
+st.subheader("caracteristicas del dataset")
+st.write(c.describe())
   
   
