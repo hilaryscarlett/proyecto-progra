d5c8f1f3de2c6117deddb870b9542a905911e46b
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-25 09:15:59 -0500

    Update 1234.py

--- 1234.py
+++ 1234.py
@@ -3,6 +3,8 @@ import streamlit as st
 import urllib.request
 import pandas as pd
 import numpy as np
+from matplotlib import pyplot as plt
+
 st.header("FALLECIDOS COVID")
 
 def download_data():
@@ -21,8 +23,13 @@ st.write(c.describe())
 #url= "https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv"
 #datos=pd.read_csv(url,sep=",")
 #st.line_chart(data=datos,x="FECHA_UTC",y="MAGNITUD")
+x=np.linspace(0,10,100)
 
-
+fig=plt.figure()
+plt.plot(x,sin(x))
+st.write(fig)
 
 st.sidebar.write("hello this is my")
+
+
 
