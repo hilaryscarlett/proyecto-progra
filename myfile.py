d3feeadc76bc7b298a95cd0c44226bf6d3862f98
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-11-25 17:58:30 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -7,7 +7,7 @@ import datetime
 import urllib.request
 
 import altair as alt
-st.sidebar.write("Fallecidos por covid")
+st.sidebar.write("UNIVERSIDAD PERUANA CAYETANO HEREDIA")
 
 st.title("Fallecidos por COVID-19")
 
