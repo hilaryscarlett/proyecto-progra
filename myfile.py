48de61d89f98168f0d15fc61384baad5c0c2a09c
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-24 21:46:45 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -42,23 +42,4 @@ st.write("Número de teléfono o email de contecto:", tittle)
 
 
 
-#$ pip install streamlit --upgrade
 
-st.header("FALLECIDOS COVID")
-
-def download_data():
-  url='https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv'
-  filename="fallecidos_covid"
-  urllib.request.urlretrieve(url,filename)
-  df=pd.read_csv("fallecidos_covid")
-  return df
-  
-c=download_data()
-st.write("dimensiones: "+str(c.shape[0])+"filas"+str(c.shape[1])+"columnas")
-st.dataframe(c)
-st.subheader("caracteristicas del dataset")
-st.write(c.describe())
-
-url= "https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv"
-datos=pd.read_csv(url,sep=",")
-st.line_chart(data=datos,x="FECHA_UTC",y="MAGNITUD")
