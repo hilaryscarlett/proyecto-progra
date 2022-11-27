5d0a3524194d7d231a3fd597ca294e0ce4b53cb8
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-27 00:47:18 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -19,9 +19,7 @@ with st.sidebar:
     selected = option_menu("Main Menu", ["Home", 'Settings'], 
         icons=['house', 'gear'], menu_icon="cast", default_index=1)
     selected
-#------------------------------------------------------------------
 
-#------------------------------------------------------------------
 
 
 st.sidebar.image('ministerio.png')        
