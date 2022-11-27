f589bb9c2fcd98bb67da622a76f5daade605b134
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-11-27 00:48:36 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -15,10 +15,12 @@ st.set_page_config(
     page_icon="🟡",
 )
 
+
 with st.sidebar:
     selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+                           icons=['house', 'gear'], menu_icon="cast", default_index=1)
     selected
+
 
 
 
