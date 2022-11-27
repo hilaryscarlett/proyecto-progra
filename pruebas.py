7b4ff7652b7b6a16fcd7f8f2f0885265389c1601
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-27 00:43:42 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from streamlit_option_menu import option_menu
-
+#$ pip install option_menu
 with st.sidebar:
   selected=option_menu(
     menu_title = "Menú",
