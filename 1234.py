e5cbd98d039b849df151ba442d438102f459aab0
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-26 22:55:59 -0500

    Update 1234.py

--- 1234.py
+++ 1234.py
@@ -9,21 +9,4 @@ st.set_page_config(
     page_icon="🟡",
 
 
-#st.header("FALLECIDOS COVID")
 
- #def download_data():
-  #url='https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv'
-  #filename="fallecidos_covid"
-  #urllib.request.urlretrieve(url,filename)
-  #df=pd.read_csv("fallecidos_covid")
-  #return df
-  
-#c=download_data()
-#st.write("dimensiones: "+str(c.shape[0])+"filas"+str(c.shape[1])+"columnas")
-#st.dataframe(c)
-#st.subheader("caracteristicas del dataset")
-#st.write(c.describe())
-
-
-#st.sidebar.write("hello this is my")
-#if st.sidebar.button("presionar aquí")st.ballons
