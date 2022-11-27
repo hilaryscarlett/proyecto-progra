7296b6848813b52fe35ffacd543657e36a2379e5
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-26 23:52:25 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -2,9 +2,18 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-Home.py
-pages/
-About.py
-2_Page_two.py
-3_😎_three.py
-  
+excel_file="fallecidos_covid.xslx"
+sheet_name="fallecidos_covid"
+df=pd.read_excel(excel_file,
+                sheet_name=sheet_name
+                usecols="D:G",
+                header=2)
+df_dato1=df.groupby(["SEXO"], as_index=false)["PROVINCIA"].count()
+df_datos12=df_dato1
+st.dataframe(df)
+st.write(df_datos12)
+pie_chart=px.pie(df_datos12,
+                 title="ola1",
+                 values="ola2",
+                 names="SEXO")
+st.plotly_chart(pie_chart)
