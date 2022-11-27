c19f9f5bc011a7802b0a2201608441c2cd60ff57
GutierrezLopez <117697526+GutierrezLopez@users.noreply.github.com>
2022-11-27 11:33:41 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -35,10 +35,12 @@ with st.sidebar:
     
 if selected == 'Inicio':
     st.markdown("<h1 style='text-align: center; color: red;'>SITUACIÓN ACTUAL COVID-19</h1>", unsafe_allow_html=True)
-    
+    st.subheader("¿Qué son los coronavirus?")
+    st.caption("La COVID-19 es una enfermedad infecciosa causada por el coronavirus SARS-CoV-2, que se propaga de persona a persona a través de gotitas, partículas acuosas o aerosoles expulsados por individuos infectados al momento de hablar, toser, estornudar, o incluso respirar. El virus puede ser inhalado por las personas que están cerca al enfermo y también puede contaminar cualquier tipo de superficie (pasamanos, mesas, lapiceros, entre otros), e ingresar al organismo cuando nos tocamos los ojos, nariz o boca con las manos sin lavar luego de haber tocado esas superficies contaminadas. Las personas mayores y las que sufren enfermedades respiratorias, diabetes o cardiopatías podrían desarrollar el virus en un nivel grave, si llegaran a contraerlo.")
+    text=st
     
 if selected == 'Datos':
-    
+  
    
     st.image('ministerio.png')
     st.markdown("<h1 style='text-align: center; color: black;'>Fallecidos por Covid-19</h1>", unsafe_allow_html=True)
