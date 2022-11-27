8a4f3cdbe3d61302317ffe4551e0d1c7320ebebe
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-11-27 02:25:35 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+def download_data():
+    url='https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv'
+    filename="fallecidos_covid"
+    urllib.request.urlretrieve(url,filename)
+    df=pd.read_csv("fallecidos_covid")
+    return df
+st.subheader("Datos generales proporcionados por el Ministerio de Salud sobre el número de fallecidos")
+c=download_data()
 
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['provincia', 'distrito'])
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['DISTRITO', 'PROVINCIA', 'SEXO'])
 
-st.map(df)
-
+st.line_chart(chart_data)
 
