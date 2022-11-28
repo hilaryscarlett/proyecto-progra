3b83ca54f1c75e73990074792fb4f7bf52e23cc7
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-11-27 23:05:42 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -87,13 +87,13 @@ if selected == 'Equipo':
     
     with col1:
         st.header("A cat")
-        st.image("https://static.streamlit.io/examples/cat.jpg")
+        st.image("cuy.png")
     with col2:
         st.header("A dog")
-        st.image("https://static.streamlit.io/examples/dog.jpg")
+        st.image("conejo.png")
     with col3:
         st.header("An owl")
-        st.image("https://static.streamlit.io/examples/owl.jpg")
+        st.image("pingui.png")
     
     
 
