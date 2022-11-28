e88d56990dec58bea59b5fb2056ae8b8853aba10
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-11-27 22:58:04 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -81,7 +81,19 @@ if selected == 'INFORMACIÓN ESTADÍSTICA':
     #st.markdown("<h1 style='text-align: center; color: black;'>Ministerio de Salud</h1>", unsafe_allow_html=True)
     
 if selected == 'Equipo':
-    st.markdown("<h1 style='text-align: center; color: black;'>Ministerio de Salud</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: black;'>Integrantes del Grupo</h1>", unsafe_allow_html=True)
+    
+    col1, col2, col3 = st.columns(3)
+    
+    with col1:
+        st.header("A cat")
+        st.image("https://static.streamlit.io/examples/cat.jpg")
+    with col2:
+        st.header("A dog")
+        st.image("https://static.streamlit.io/examples/dog.jpg")
+    with col3:
+        st.header("An owl")
+        st.image("https://static.streamlit.io/examples/owl.jpg")
     
     
 
