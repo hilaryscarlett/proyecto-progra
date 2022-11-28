7db62e262ef7f7b57d370f0fe1c66477037b2c01
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-11-27 23:09:32 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -87,13 +87,13 @@ if selected == 'Equipo':
     
     with col1:
         st.header("A cat")
-        st.image("cuy.png")
+        st.image("cuy.jpg")
     with col2:
         st.header("A dog")
-        st.image("conejo.png")
+        st.image("conejo.jpg")
     with col3:
         st.header("An owl")
-        st.image("pingui.png")
+        st.image("pingui.jpg")
     
     
 
