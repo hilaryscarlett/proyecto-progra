be65a0ab585d795f7188920c6681ee03c5abc60d
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-12-02 09:24:24 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -73,34 +73,6 @@ if selected == 'Inicio':
     
     st.caption("Fuente: Instituto Nacional de Salud y Centro Nacional de Epidemiología, Prevención y Control de Enfermedades - MINSA")
     
-    data = ["Lima Metropolitana","Arequipa","Piura","La Libertad","Callao","Ancash","Junin","Cusco","Lima Region","Lambayeque","Ica","Cajamarca","Puno","San Martin","Loreto","Tacna","Moquegua","Huanuco","Ayacucho","Amazonas","Apurimac","Ucayali","Tumbes","Huancavelica","Pasco","Madre de Dios"]
-    plt.bar([1840931,271943,174542,170320,162850,150853,147972,137842,130950,121638,120560,109382,75258,67640,63474,61241,59850,58493,54119,50107,44592,43886,31817,29094,27566,20526], data)
-    plt.show()
-
-
-
-
-    #EN OBSERVACIÓN---------------------------------------
-    positividad= [10.98,9.22,14.57]
-    provincias = ["Lima Metropolitana","Arequipa","Piura"]
-    plt.pie(positividad, labels=provincias)
-    plt.axis("equal")
-    plt.show()
-    
-    st.subheader("GRÁFICA DE CASOS CONFIRMADOS")
-    fig = plt.figure(u'Gráfica de barras') # Figure
-    ax = fig.add_subplot(111) # Axes
-    positividad= [1840931,271943,174542,170320,162850,150853,147972,137842,130950,121638,120560,109382,75258,67640,63474,61241,59850,58493,54119,50107,44592,43886,31817,29094,27566,20526]
-    provincias = ["Lima Metropolitana","Arequipa","Piura","La Libertad","Callao","Ancash","Junin","Cusco","Lima Region","Lambayeque","Ica","Cajamarca","Puno","San Martin","Loreto","Tacna","Moquegua","Huanuco","Ayacucho","Amazonas","Apurimac","Ucayali","Tumbes","Huancavelica","Pasco","Madre de Dios"]
-    xx = range(len(provincias))
-    ax.bar(xx, positividad, width=1, align='center')
-    ax.set_xticks(xx)
-    ax.set_xticklabels(provincias,rotation="vertical")
-    plt.show()
-    st.pyplot(plt)
-    
-    st.caption("Elaboración propia con datos del Ministerio de salud")
-    #_-----------------------------------------------------------
     st.subheader("FALLECIDOS POR COVID 19")
  
     
