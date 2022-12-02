54506d4646a51a4784c9f296c583f9c7246f74b7
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-12-01 20:44:36 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -70,6 +70,7 @@ if selected == 'Inicio':
     
     text=st
     
+    
 if selected == 'Datos':
     st.image('ministerio.png')
     st.markdown("<h1 style='text-align: center; color: black;'>Fallecidos por Covid-19</h1>", unsafe_allow_html=True)
