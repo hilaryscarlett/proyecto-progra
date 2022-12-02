4360647ba659ab891559ee6811903298aaacdd5b
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-12-01 19:14:07 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -42,12 +42,20 @@ if selected == 'Inicio':
     st.caption("Una enfermedad infecciosa causada por el coronavirus SARS-CoV-2, que se propaga de persona a persona a través de gotitas, partículas acuosas o aerosoles expulsados por individuos infectados al momento de hablar, toser, estornudar, o incluso respirar. El virus puede ser inhalado por las personas que están cerca al enfermo y también puede contaminar cualquier tipo de superficie (pasamanos, mesas, lapiceros, entre otros), e ingresar al organismo cuando nos tocamos los ojos, nariz o boca con las manos sin lavar luego de haber tocado esas superficies contaminadas. Las personas mayores y las que sufren enfermedades respiratorias, diabetes o cardiopatías podrían desarrollar el virus en un nivel grave, si llegaran a contraerlo.")
     st.image('covid19.jpg')
     st.subheader("SINTOMAS:")
-    st.caption("- Fiebre") 
-    st.caption("- Tos seca")
-    st.caption("- Cansansio") 
-    st.caption("- Dolor de garganta")
-    st.caption("- Dificultad para respirar") 
-    st.caption("- Congestión nasal")
+    
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        st.caption("- Fiebre") 
+        st.caption("- Tos seca")
+        st.caption("- Cansansio") 
+        st.caption("- Dolor de garganta")
+        st.caption("- Dificultad para respirar") 
+        st.caption("- Congestión nasal")
+    with col2:
+        st.image("sintomas.jpg")
+ 
+    st.subheader("Casos positivos de Covid-19")
     text=st
     
 if selected == 'Datos':
