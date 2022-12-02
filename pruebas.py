b3ca1c339ec16452c98c80cd8d042ed5529bf6ac
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-01 20:38:12 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -27,7 +27,7 @@ datos=pd.read_csv(url,sep=",")
 st.line_chart(data=datos, x='FECHA_FALLECIMIENTO',y="EDAD_DECLARADA")
 
 st.header("intento 1")
-st.area_chart(data=datos, x="EDAD_DECLARADA",y="SEXO")
+st.area_chart(data=datos,"*", x="SEXO",y="EDAD_DECLARADA")
 
 st.subheader("")
 
