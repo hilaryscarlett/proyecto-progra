28103dc3f4bf83a7c760dcf48d1b25109dfc41cc
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-12-01 22:47:38 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -66,10 +66,12 @@ if selected == 'Inicio':
     
     d=download()
     st.dataframe(d)
-    st.plot(d)
+    
     
     st.caption("Fuente: Instituto Nacional de Salud y Centro Nacional de Epidemiología, Prevención y Control de Enfermedades - MINSA")
     
+    
+    #EN OBSERVACIÓN---------------------------------------
     positividad= [1840931,271943,174542,170320,162850,150853,147972,137842,130950,121638,120560,109382,75258,67640,63474,61241,59850,58493,54119,50107,44592,43886,31817,29094,27566,20526]
     provincias = ["Lima Metropolitana","Arequipa","Piura","La Libertad","Callao","Ancash","Junin","Cusco","Lima Region","Lambayeque","Ica","Cajamarca","Puno","San Martin","Loreto","Tacna","Moquegua","Huanuco","Ayacucho","Amazonas","Apurimac","Ucayali","Tumbes","Huancavelica","Pasco","Madre de Dios"]
     plt.pie(positividad, labels=provincias, autopct="%0.1f %%")
@@ -89,7 +91,7 @@ if selected == 'Inicio':
     st.pyplot(plt)
     
     st.caption("Elaboración propia con datos del Ministerio de salud")
-    
+    #_-----------------------------------------------------------
     st.subheader("FALLECIDOS POR COVID 19")
  
     
