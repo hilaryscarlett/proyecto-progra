7a15e6d45a7d266057be9f5b10d08ef982e69f9c
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-12-01 19:59:05 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -58,12 +58,14 @@ if selected == 'Inicio':
         
     st.subheader("CASOS POSITIVOS DE COVID-19")
     def download_data():
-       
+        url='https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/Casospositivos.csv'
         filename="Casospositivos"
+        urllib.request.urlretrieve(url,filename)
         df=pd.read_csv("Casospositivos")
         return df
     st.subheader("Datos generales proporcionados por el Ministerio de Salud sobre el número de fallecidos")
     d=download_data()
+    st.dataframe(d)
  
     
     text=st
