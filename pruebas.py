8648e1d0975d17941e2e901b48b4f684cea964ac
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-02 10:20:43 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -52,8 +52,8 @@ chart_data = pd.DataFrame(
 st.area_chart(chart_data)
 
 
-st.header('Ejemplo de mapa')
-df = pd.DataFrame(
-    np.random.randn(1000, 2) /[50, 50]+ [-13.15, -74.22] + [-12.03, -77.04],
-    columns=['lat', 'lon'])
-st.map(df)
+#st.header('Ejemplo de mapa')
+#df = pd.DataFrame(
+#    np.random.randn(1000, 2) /[50, 50]+ [-13.15, -74.22] + [-12.03, -77.04],
+#    columns=['lat', 'lon'])
+# st.map(df)
