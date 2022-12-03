e3cfcfb60be749b5af34d932b600bfae691144b7
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-12-03 00:47:48 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -45,7 +45,10 @@ if selected == 'Inicio':
     
     col1, col2 = st.columns(2)
     
+    st.subheader("SÍNTOMAS")
     with col1:
+        st.caption("Según la OMS, La COVID-19 afecta a diferentes personas de forma distinta. La mayoría de las personas infectadas desarrollarán una enfermedad de leve a moderada y se recuperarán sin necesidad de hospitalización.") 
+        st.caption("*Síntomas más comunes:*")
         st.caption("- Fiebre") 
         st.caption("- Tos seca")
         st.caption("- Cansansio") 
