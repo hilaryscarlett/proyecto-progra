51fd118bf04130f57cce8371ba768890df441c5b
melaniemalca16 <103445979+melaniemalca16@users.noreply.github.com>
2022-12-03 00:31:02 -0500

    Update myfile.py

--- myfile.py
+++ myfile.py
@@ -31,7 +31,7 @@ with st.sidebar:
     )
     
 if selected == 'Inicio':
-    st.markdown("<h1 style='text-align: center; color: red;'>SITUACIÓN ACTUAL COVID-19</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: red;'>COVID-19</h1>", unsafe_allow_html=True)
     st.subheader("¿QUÉ ES LA COVID-19?")
     st.caption("Una enfermedad infecciosa causada por el coronavirus SARS-CoV-2, que se propaga de persona a persona a través de gotitas, partículas acuosas o aerosoles expulsados por individuos infectados al momento de hablar, toser, estornudar, o incluso respirar. El virus puede ser inhalado por las personas que están cerca al enfermo y también puede contaminar cualquier tipo de superficie (pasamanos, mesas, lapiceros, entre otros), e ingresar al organismo cuando nos tocamos los ojos, nariz o boca con las manos sin lavar luego de haber tocado esas superficies contaminadas. Las personas mayores y las que sufren enfermedades respiratorias, diabetes o cardiopatías podrían desarrollar el virus en un nivel grave, si llegaran a contraerlo.")
     st.image('covid11.png')
@@ -50,7 +50,11 @@ if selected == 'Inicio':
         
     with col2:
         st.image("sintomas.jpg",width=None, use_column_width="always")
+        
+    st.markdown("<h1 style='text-align: center; color: green;'>SITUACIÓN ACTUAL COVID-19</h1>", unsafe_allow_html=True)
+    st.subheader("CASOS POSITIVOS DE COVID-19 EN PERÚ")
     st.image("minsa.jpeg")   
+    st.caption("Fuente: Ministerio de Salud")
     st.subheader("CASOS POSITIVOS DE COVID-19 EN PERÚ")
     st.caption("Número total de muestras: 36 376 044")
     st.caption("Total de casos confirmados: 4 227 446")
