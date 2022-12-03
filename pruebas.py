99ec8b4d61bcc24317ba19da3c712865d694408d
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-02 19:41:49 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -33,12 +33,14 @@ st.write()
 
 st.subheader("HOLA")
 def lectura(a):
-    df = pd.read_csv(a)
+    df = pd.read_csv(a, encoding = 'latin1')
     names = ['FECHA_CORTE','FECHA_FALLECIMIENTO','EDAD_DECLARADA','SEXO','CLASFIFICACION_DEF','DEPARTAMENTO','PROVINCIA','DISTRITO','UBIGEO','UUID']
     df.columns = names
     return df
 
 st.write(lectura('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv'))
+
+
 
 import pandas as pd
 import numpy as np
