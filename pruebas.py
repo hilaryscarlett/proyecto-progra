132814b51f8bf71fb5ab794bfa1281e6fdb0bc21
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-04 02:29:53 -0500

    Update pruebas.py

--- pruebas.py
+++ pruebas.py
@@ -24,11 +24,27 @@ import urllib.request
 dfprueba=pd.read_csv('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid%20(6).csv')
 st.write(dfprueba.reset_index())
 
-option = st.selectbox('ingresar criterio',('UBIGEO','UUID'))     #STRING QUE GUARDA MI SELECCION ENTRE COMILLAS
+option = st.selectbox('ingresar mayor, menor o igual para ver fechas',('=','<','>'))     #STRING QUE GUARDA MI SELECCION ENTRE COMILLAS
 st.write(dfprueba[option])
-
 number = st.number_input('Insertar numero')
-st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] > number])
+#st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] > number])
+
+
+
+if option == "=":
+    number = st.number_input('Insertar numero')
+    st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] == number])
+if option == "<":
+    number = st.number_input('Insertar numero')
+    st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] < number])
+if option ==">":
+    number = st.number_input('Insertar numero')
+    st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] > number])
+    
+    
+
+
+
 
 
 #st.subheader("PRUEBA AREAS")
