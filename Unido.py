c2027bc386364bcc41b3230be9e436ec99786409
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-04 03:26:54 -0500

    Update Unido.py

--- Unido.py
+++ Unido.py
@@ -2,13 +2,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from datetime import time
-import datetime
-import urllib.request
 
 
-
-df_prueba = lectura('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv') 
+df_prueba = pd.read_csv('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv') 
 st.write(df_prueba)
 
 st.write(df_prueba.iloc[0])
