e86e2b7841b3818bc2e6938257f0592e1a4e300d
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-04 02:40:17 -0500

    Update SELECCION.py

--- SELECCION.py
+++ SELECCION.py
@@ -2,11 +2,16 @@ import numpy as np
 import pandas as pd
 
 
-#def lectura(a):
-    #df = pd.read_csv(a, parse_dates = ['FECHA_CORTE', 'FECHA_FALLECIMIENTO'])
-    #return df
+dfprueba=pd.read_csv('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid%20(6).csv')
+#st.write(dfprueba.reset_index())
 
-df_prueba=pd.read_csv('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid_2.csv')
+option = st.selectbox('ingresar mayor, menor o igual para ver fechas',('=','<','>'))     #STRING QUE GUARDA MI SELECCION ENTRE COMILLAS
 
-option = st.selectbox('INGRESAR CRITERIO',('SEXO','EDAD_DECLARADA')) 
-st.write(df_prueba2[option])
+number = st.number_input('Insertar numero')
+
+if option == "=":
+    st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] == number])
+if option == "<":
+    st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] < number])
+if option ==">":
+    st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] > number])
