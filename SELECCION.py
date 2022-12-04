57b8ee7d7adce8e5ff55d9d651816044a0b3a130
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-04 02:48:52 -0500

    Update SELECCION.py

--- SELECCION.py
+++ SELECCION.py
@@ -4,14 +4,13 @@ import pandas as pd
 
 dfprueba=pd.read_csv('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid%20(6).csv')
 #st.write(dfprueba.reset_index())
-
-option = st.selectbox('mayor - menor - igual para ver fechas',('=','<','>'))     #STRING QUE GUARDA MI SELECCION ENTRE COMILLAS
+option = st.selectbox('mayor - menor - igual para ver fechas',('igual','menor','mayor'))
 
 number = st.number_input('Insertar numero')
 
-if option == '=':
+if option == 'igual':
     st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] == number])
-if option == '<':
+if option == 'menor':
     st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] < number])
-if option == '>':
+if option == 'mayor':
     st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] > number])
