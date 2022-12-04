3c0a6e1a4afb9516f5d3836bd35e5742ed05306b
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-04 00:00:38 -0500

    Update SELECCION.py

--- SELECCION.py
+++ SELECCION.py
@@ -6,7 +6,6 @@ def lectura(a):
     df = pd.read_csv(a, parse_dates = ['FECHA_CORTE', 'FECHA_FALLECIMIENTO'])
     return df
 
-st.subheader("escoje departamento")
 df_prueba2=lectura('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid_2.csv')
 
 option = st.selectbox('ingresar criterio',('LIMA','AMAZONAS'))     #STRING QUE GUARDA MI SELECCION ENTRE COMILLAS
