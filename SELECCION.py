0236bccacbb957f1faca47626db4bda548be655f
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-04 02:55:45 -0500

    Update SELECCION.py

--- SELECCION.py
+++ SELECCION.py
@@ -4,14 +4,15 @@ import pandas as pd
 
 dfprueba=pd.read_csv('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv')
 
-option = st.selectbox('mayor - menor - igual para ver fechas',('igual','menor','mayor'))
-st.write(option)
+title = st.text_input('igual', 'mayor','menor')
+
+st.write('usted escogio', title)
 
 number = st.number_input('Insertar numero')
 
-if option == 'igual':
+if title == 'igual':
     st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] == number])
-if option == 'menor':
+if title == 'menor':
     st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] < number])
-if option == 'mayor':
+if title == 'mayor':
     st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] > number])
