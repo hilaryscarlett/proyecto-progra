675b0103025f387d789a6927e28f430df2d2d8f1
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-04 03:22:52 -0500

    Delete SELECCION.py

--- SELECCION.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import pandas as pd
-
-
-dfprueba=pd.read_csv('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv')
-st.write(dfprueba.iloc[0])
