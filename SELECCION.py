aff50a5b324ab3d20f629b932d63bdeaeb16057d
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-04 03:21:38 -0500

    Update SELECCION.py

--- SELECCION.py
+++ SELECCION.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 
 dfprueba=pd.read_csv('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv')
-st.write(dfprueba.loc[23])
+st.write(dfprueba.iloc[23])
