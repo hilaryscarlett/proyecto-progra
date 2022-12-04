60dcf3cf541687f2829cfe7dfbf92e49e5581aa3
hilaryscarlett <117694736+hilaryscarlett@users.noreply.github.com>
2022-12-04 03:20:58 -0500

    Update SELECCION.py

--- SELECCION.py
+++ SELECCION.py
@@ -3,16 +3,4 @@ import pandas as pd
 
 
 dfprueba=pd.read_csv('https://raw.githubusercontent.com/hilaryscarlett/proyecto-progra/main/fallecidos_covid.csv')
-
-title = st.text_input('jola')
-
-st.write('usted escogio', title)
-
-number = st.number_input('Insertar numero')
-
-if title == 'igual':
-    st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] == number])
-if title == 'menor':
-    st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] < number])
-if title == 'mayor':
-    st.write(dfprueba.loc[dfprueba['EDAD_DECLARADA'] > number])
+st.write(dfprueba.loc[23])
